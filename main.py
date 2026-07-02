38934811ee6dab2b03ff80a6f55a920ed888c41d
Harshalzarikar <harshalzarikar14@gmail.com>
2026-07-02 16:02:09 +0530

    Make pysqlite3 override optional

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # Override sqlite3 for Render compatibility with ChromaDB
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+try:
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
 
 from fastapi import FastAPI, Request, HTTPException, Query
 import uvicorn
